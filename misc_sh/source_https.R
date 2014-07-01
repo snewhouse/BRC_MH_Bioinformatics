0ee0faafefe36066487c3f23e62660929a4ab4a9
Dr Stephen Newhouse <stephen.j.newhouse@gmail.com>
2014-07-01 13:56:00 +0100

    source https R

---
+++ misc_sh/source_https.R
@@ -0,0 +1,9 @@
+source_https <- function(url, ...) {
+  # load package
+  require(RCurl)
+ 
+  # parse and evaluate each .R script
+  sapply(c(url, ...), function(u) {
+    eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
+  })
+}
